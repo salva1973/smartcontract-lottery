11e8c8b3c61a24f4a14254d2a3b76ab29cad8ae4
Salvatore Vivolo <3092390+salva1973@users.noreply.github.com>
2022-03-19 10:49:51 +0100

    bulding unit testing

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -1,11 +1,12 @@
 from scripts.helpful_scripts import get_account, get_contract, fund_with_link
 from brownie import Lottery, network, config
+import time
 
 
 def deploy_lottery():
   # account = get_account(id="freecodecamp-account")
   account = get_account()
-  Lottery.deploy(
+  lottery = Lottery.deploy(
     get_contract("eth_usd_price_feed").address,
     get_contract("vrf_coordinator").address,
     get_contract("link_token").address,
@@ -15,6 +16,7 @@ def deploy_lottery():
     publish_source=config["networks"][network.show_active()].get("verify", False),
   )
   print("Deployed lottery!")
+  return lottery
   
   
 def start_lottery():
@@ -43,9 +45,12 @@ def end_lottery():
   tx.wait(1)
   ending_transaction = lottery.endLottery({"from": account})  
   ending_transaction.wait(1)
+  time.sleep(60)
+  print(f"{lottery.recentWinner()} is the new winner!")
   
   
 def main(): 
   deploy_lottery()
   start_lottery()
   enter_lottery()
+  end_lottery()
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -32,9 +32,10 @@ def get_account(index=None, id=None):
   
   return accounts.add(config["wallets"]["from_key"]) # default
 
-contract_to_mock = {"eth_usd_price_feed": MockV3Aggregator,
-                    "vrf_coordinator": VRFCoordinatorMock,
-                    "link_token": LinkToken,
+contract_to_mock = {
+  "eth_usd_price_feed": MockV3Aggregator,
+  "vrf_coordinator": VRFCoordinatorMock,
+  "link_token": LinkToken,
 }
 
 def get_contract(contract_name):
@@ -72,7 +73,7 @@ def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
   VRFCoordinatorMock.deploy(link_token.address, {"from": account})
   print("Deployed!")
   
-def fund_with_link(contract_address, account=None, link_token=None, amount=100000000000000000): # 0.1 Link
+def fund_with_link(contract_address, account=None, link_token=None, amount=250000000000000000): # 0.1 Link                                                                           
   account = account if account else get_account()
   link_token = link_token if link_token else get_contract("link_token")
   tx = link_token.transfer(contract_address, amount, {"from": account})
